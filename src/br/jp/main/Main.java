91046bb0238c41a86c05184d88dd075f04d97c05
ramirezjpdf <ramirez.jpdf@ymail.com>
2014-01-22 21:10:58 -0200

    Main class for run the program

---
+++ src/br/jp/main/Main.java
@@ -0,0 +1,17 @@
+package br.jp.main;
+
+import java.io.IOException;
+
+import br.jp.processManager.ProcessManager;
+import br.jp.userInterface.CommandLineInterface;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		ProcessManager pm = new ProcessManager();
+		CommandLineInterface cl = new CommandLineInterface(System.in, pm);
+		cl.runPrompt();
+
+	}
+
+}
